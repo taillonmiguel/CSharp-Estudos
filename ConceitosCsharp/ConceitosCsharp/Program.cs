1a5dc9a7f9024463b1ba1c0e5c874550b0c41ee1
Taillon Miguel <taillonmiguel@live.com>
2022-03-18 18:17:14 -0400

    alteracao texto

--- ConceitosCsharp/ConceitosCsharp/Program.cs
+++ ConceitosCsharp/ConceitosCsharp/Program.cs
@@ -6,7 +6,7 @@ namespace ConceitosCsharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Oloquinho meu");
         }
     }
 }
