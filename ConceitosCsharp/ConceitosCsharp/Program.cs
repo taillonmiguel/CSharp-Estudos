2070307e87eacf61c79d5a10fd8f82f5761487ea
Taillon Miguel <taillonmiguel@live.com>
2022-03-18 16:30:32 -0400

    Conceitos do Csharp

---
+++ ConceitosCsharp/ConceitosCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConceitosCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
